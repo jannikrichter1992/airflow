1c177be5b879b7a8a774172217dc102101c60247
Jannik Richter <richter@office.weclapp.com>
2022-12-20 19:27:04 +0100

    renamed py task

--- helloworld.py
+++ helloworld.py
@@ -17,7 +17,7 @@ dag = DAG('hello_world_prep', description='Hello World DAG',
           start_date=datetime(2017, 3, 20), catchup=False)
 
 python_task = PythonVirtualenvOperator(
-        task_id='task1',
+        task_id='python_task',
         python_callable=print_hello,
         requirements=get_modules(),
         #python_version='3.8',
