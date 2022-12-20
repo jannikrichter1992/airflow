89b33dcf9cb3881c5fb13e04359b16310d5691e7
Jannik Richter <richter@office.weclapp.com>
2022-12-20 19:03:49 +0100

    Virtual Env

--- helloworld.py
+++ helloworld.py
@@ -1,19 +1,25 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.python_operator import PythonOperator
-import subprocess
+from airflow.operators.python_operator import PythonVirtualenvOperator
 
-def install_modules():
-    subprocess.call(['python', '-m', 'venv' ,'/opt/airflow/dags/helloworld_venv'])
-    subprocess.call(['bash'])
-    subprocess.call(['source', '/opt/airflow/dags/helloworld_venv/bin/activate'])
-    subprocess.call(['pip', 'install', '-r', 'helloworld_modules.txt'])
+def print_hello():
+    requirments = open("helloworld_modules.txt", "r")
+    lines = requirments.readlines()
+    return lines
 
 dag = DAG('hello_world_prep', description='Hello World DAG',
           schedule_interval='@once',
           start_date=datetime(2017, 3, 20), catchup=False)
 
-hello_operator = PythonOperator(task_id='hello_prep_task', python_callable=install_modules, dag=dag)
+hello_operator = PythonVirtualenvOperator(
+        task_id='hello_prep_task',
+        python_callable=print_hello,
+        requirements=print_hello(),
+        python_version='3.8',
+        provide_context=True,
+        dag=dag
+    )
+
 
 hello_operator
